0fa6c1e2afebce62b6311d25478d6ac110fde9c3
Joohan Lee <joohan@ascend.io>
2023-07-28 23:05:20 -0400

    Checkpoint in the middle of the lecture

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // Track the connection
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('=> using existing database connection');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+          dbName: "share_prompt",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        isConnected = true;
+        console.log("MongoDB connected")
+
+    } catch (error) {
+
+    }
+}
